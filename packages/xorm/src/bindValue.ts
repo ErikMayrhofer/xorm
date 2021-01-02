b3a23ce1dd52400a9077fa115f325ad4a6fa685e
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-01-02 19:19:56 +0100

    add reset

--- packages/xorm/src/bindValue.ts
+++ packages/xorm/src/bindValue.ts
@@ -5,60 +5,6 @@ function setNewFormValue(obj: any, name: string, value: any) {
   obj[name] = value;
 }
 
-/*
-{
-  tree: {
-
-  }
-  inputs: [
-
-  ]
-}
-
-
-*/
-export function htmlElementToModel(node: HTMLElement) {
-  let tree = {};
-  let inputs = [];
-
-  for (let i = 0; i < node.children.length; i++) {
-    let elem = node.children[i];
-    if ("value" in elem && "name" in elem && "addEventListener" in elem) {
-      let inputElement = elem as HTMLInputElement;
-      inputs.push(inputElement);
-
-      let name = inputElement.name;
-      let path = name.split(".");
-
-      let treeElement = {};
-      let currentLeaf = treeElement;
-      for (let element of path) {
-        currentLeaf[element] = {};
-        currentLeaf = currentLeaf[element];
-      }
-      currentLeaf[path[path.length - 1]] = inputElement;
-      tree = Object.assign(tree, treeElement);
-
-      //inputElement.addEventListener("input", updateView);
-
-      //let initThisValue = initValue[inputElement.name]; //TODO Deep access via dot-notation in obj
-      //if (initThisValue !== undefined) {
-      //inputElement.value = initThisValue;
-      //} else {
-      //setNewFormValue(initValue, inputElement.name, inputElement.value);
-      //}
-      //unregisterMemory.push({
-      //element: inputElement,
-      //listener: updateView,
-      //});
-    }
-  }
-  return {
-    tree,
-    inputs,
-  };
-}
-
 export function valueSubject(node: HTMLElement, subject: BehaviorSubject<any>) {
   console.log("Attaching FormView to ", node);
 
--- packages/xorm/src/integrations/svelte.ts
+++ packages/xorm/src/integrations/svelte.ts
@@ -1,6 +1,24 @@
 import { FormView } from "..";
 import { valueSubject } from "../bindValue";
 
+function findNameIn(
+  node: Element,
+  name: string
+): { name: string; value: string } {
+  if ("name" in node && "value" in node) {
+    if (node["name"] == name) {
+      return node;
+    }
+  }
+  for (let i = 0; i < node.children.length; i++) {
+    let result = findNameIn(node.children[i], name);
+    if (result != null) {
+      return result;
+    }
+  }
+  return null;
+}
+
 export function htmlFormView(node: HTMLElement, view: FormView) {
   let unregisterLastModel = null;
   view.$model.subscribe((newFormModel) => {
@@ -12,5 +30,8 @@ export function htmlFormView(node: HTMLElement, view: FormView) {
 
   view.$activeDataChange.subscribe((newHtmlData) => {
     console.log("Active Data Change HTML TO: ", newHtmlData);
+    for (let key in newHtmlData) {
+      findNameIn(node, key).value = newHtmlData[key];
+    }
   });
 }
